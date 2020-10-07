c864566ca0f9e2778a42c4e6bc37418b37fb16c5
Joaquin Caporalini <joaquicaporalini@gmail.com>
2020-10-07 19:20:30 -0300

    Ejercicio 4

--- plancha1/ejer4.c
+++ plancha1/ejer4.c
@@ -10,5 +10,24 @@ b ^ a = b
 b ^ a = a
 b ^ a = b
 */
+void swap3 (int * a, int * b, int * c){
+  //swap a <-> b
+  *b = *b ^ *a;
+  *a = *b ^ *a;
+  *b = *b ^ *a;
+  //swap a <-> c
+  *c = *c ^ *a;
+  *a = *c ^ *a;
+  *c = *c ^ *a;
+}
 
+
+int main (){
+  int a = -1;
+  int b = 2;
+  int c = -32768;
+  swap3(&a, &b, &c);
+  printf("%d %d %d\n",a,b,c);
+  return 0;
+}
 
