52686cce9484d08c178ece5ca5a9dd88692d6370
Joaquin Caporalini <joaquicaporalini@gmail.com>
2020-10-19 18:16:21 -0300

    faltan acomodar enunciados con dirs de memoria

--- plancha2/ejer6.s
+++ plancha2/ejer6.s
@@ -1,15 +1,56 @@
 .data
   format: .asciz "%ld\n"
+  format1: .asciz "%lx\n"
   i:
     .quad 0xDEADBEEF
 .text
 .global main
 main:
+  movq $format, %rdi  # El primer argumento es el formato.
+  movq $1234, %rsi    # El valor a imprimir.
+  xorq %rax, %rax     # Cantidad de valores de punto flotante.
+  call printf
+  
+  # Parte (a)
   movq $format, %rdi
-  movq $1234, %rsi
+  movq %rsp, %rsi
   xorq %rax, %rax
   call printf
-  # El primer argumento es el formato.
-  # El valor a imprimir.
-  # Cantidad de valores de punto flotante.
+
+  # Parte (b) #!
+  movq $format, %rdi
+  movq $format, %rsi
+  xorq %rax, %rax
+  call printf
+
+  # Parte (c) #!
+  movq $format1, %rdi
+  movq $format, %rsi
+  xorq %rax, %rax
+  call printf
+
+  # Parte (d)
+  movq $format, %rdi
+  movq %rsp, %rsi
+  xorq %rax, %rax
+  call printf
+
+  # Parte (e)
+  movq $format, %rdi
+  movq 8(%rsp), %rsi
+  xorq %rax, %rax
+  call printf  
+
+  # Parte (f)
+  movq $format, %rdi
+  movq i, %rsi
+  xorq %rax, %rax
+  call printf 
+
+  # Parte (g)
+  movq $format, %rdi
+  movq i, %rsi
+  xorq %rax, %rax
+  call printf 
+
   ret
