f03a1b29caf53ff0ab341e226a2baa3ed3a25aa1
Joaquin Caporalini <joaquicaporalini@gmail.com>
2020-10-21 10:45:59 -0300

    Remplazo palabra reservada

--- plancha2/ejer6.s
+++ plancha2/ejer6.s
@@ -1,54 +1,54 @@
 .data
-  format: .asciz "%ld\n"
-  format1: .asciz "%lx\n"
+  formato: .asciz "%ld\n"
+  formato1: .asciz "%lx\n"
   i:
     .quad 0xDEADBEEF
 .text
 .global main
 main:
-  movq $format, %rdi  # El primer argumento es el formato.
+  movq $formato, %rdi  # El primer argumento es el formato.
   movq $1234, %rsi    # El valor a imprimir.
   xorq %rax, %rax     # Cantidad de valores de punto flotante.
   call printf
   
   # Parte (a)
-  movq $format, %rdi
+  movq $formato, %rdi
   movq %rsp, %rsi
   xorq %rax, %rax
   call printf
 
   # Parte (b) #!
-  movq $format, %rdi
-  movq $format, %rsi
+  movq $formato, %rdi
+  movq $formato, %rsi
   xorq %rax, %rax
   call printf
 
   # Parte (c) #!
-  movq $format1, %rdi
-  movq $format, %rsi
+  movq $formato1, %rdi
+  movq $formato, %rsi
   xorq %rax, %rax
   call printf
 
   # Parte (d)
-  movq $format, %rdi
+  movq $formato, %rdi
   movq %rsp, %rsi
   xorq %rax, %rax
   call printf
 
   # Parte (e)
-  movq $format, %rdi
+  movq $formato, %rdi
   movq 8(%rsp), %rsi
   xorq %rax, %rax
   call printf  
 
   # Parte (f)
-  movq $format, %rdi
+  movq $formato, %rdi
   movq i, %rsi
   xorq %rax, %rax
   call printf 
 
   # Parte (g)
-  movq $format, %rdi
+  movq $formato, %rdi
   movq i, %rsi
   xorq %rax, %rax
   call printf 
