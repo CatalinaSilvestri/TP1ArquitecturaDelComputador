b623b5ffef5bafccbf76ba72c42aa7e2273e13eb
Joaquin Caporalini <joaquicaporalini@gmail.com>
2020-10-27 11:26:58 -0300

    Comentarios y retorno

--- plancha2/ejer7_b.s
+++ plancha2/ejer7_b.s
@@ -2,20 +2,24 @@
 .global main
 main:
   xorb %dl, %dl
-  movb $64, %cl
-  movq $0xffffffffffffffff, %rax
+  movb $64, %cl				#Contador
+  movq $0xffffffffffffffff, %rax	#Numero a Contar
   
   while:
+    #Condicion de Parada
     cmpb $0, %cl
     je final
 
-    
+    #Cuerpo del while
     rolq $1, %rax
     adcb $0, %dl
 
-    dec %cl
-    jmp while
+    #Reinicio del buble
+    dec %cl     #decremento del contador
+    jmp while   #salta al inicio del loop
 
   final:
+    xorq %rax, %rax
+    movb %dl, %al	#retornar 1's contados
     ret
   
