8778998239df1df4ef394976b12ebad085776319
CatalinaSilvestri <catasilvestri@gmail.com>
2020-10-29 21:12:38 -0300

    La solucion al infierno

---
+++ plancha2/ejer9.s
@@ -0,0 +1,39 @@
+.data
+fmt: .string "%d"
+entero: .long -100
+funcs:  .quad f1
+        .quad f2
+        .quad f3
+
+.text
+f1:     movl $0, %esi; movq $fmt, %rdi; call printf; jmp fin
+f2:     movl $1, %esi; movq $fmt, %rdi; call printf; jmp fin
+f3:     movl $2, %esi; movq $fmt, %rdi; call printf; jmp fin
+
+.global main
+
+main:
+        #Inicializacion de la pila
+        pushq %rbp
+        movq %rsp, %rbp
+
+        #Leemos el entero
+        movq $entero, %rsi
+        movq $fmt, %rdi
+        xorq %rax, %rax
+        call scanf      #scanf(fmt,&entero)
+
+        xorq %rax, %rax
+
+
+        movl entero, %ebx
+        movq funcs(,%rbx,8), %rdx
+
+
+        jmp *%rdx #call funcs (Sin modificar la pila)
+
+fin:
+        #recuperar pila
+        movq %rbp, %rsp
+        popq %rbp
+        ret
