8867f3a5f93d153038f30aa5e594f45311fa9901
CatalinaSilvestri <catasilvestri@gmail.com>
2020-11-09 11:48:18 -0300

    Limpiar codigo

--- plancha2/mis_saltos/mis_saltos.h
+++ plancha2/mis_saltos/mis_saltos.h
@@ -2,14 +2,14 @@
 #define __MIS_SALTOS__
 
   typedef struct _salvarEstado{
-    long int rbx, rpb, rsp, r10, r11, r12, r13, r14, r15;
-    int estado_salto;
+    long int rbx, rbp, rsp, r10, r11, r12, r13, r14, r15;
+    long int estado_salto; // para que sirve?
   } salvarEstado;
 
   typedef salvarEstado miBuf_jump[1];
 
-  int set_jump2(miBuf_jump buf);
+  int setjmp2(miBuf_jump buf);
 
-  void long_jump2(miBuf_jump buf, int);
+  void longjmp2(miBuf_jump buf, int retorno);
 
 #endif /* __MIS_SALTOS__ */
--- plancha2/mis_saltos/mis_saltos.s
+++ plancha2/mis_saltos/mis_saltos.s
@@ -1,22 +1,22 @@
 .text
 
-.global set_jump2     #Setea un salto
-set_jump2:
+.global setjmp2     #Setea un salto
+setjmp2:
 
-  #Salvando registros "callee safe"
+  #Salvando registros "callee saved"
 
   #Stack y situacion
-  movq %rbp,8(%rdi)    #rbp ->El solo echo de descomentarlo genera un seg fault.
+  movq %rbp,8(%rdi)    #rbp
   movq (%rsp), %rax     #Evitar doble lectura a mem.    
   movq %rax, 16(%rdi)  #rsp    
   
-  #Proposito general "callee safe"
+  #Proposito general "callee saved"
   movq %rbx,  (%rdi)    #rbx
   movq %r10, 24(%rdi)  #r10
   movq %r11, 32(%rdi)  #r11
   movq %r12, 40(%rdi)  #r12
   movq %r13, 48(%rdi)  #r13
-  movq %r14, 54(%rdi)  #r14
+  movq %r14, 56(%rdi)  #r14
   movq %r15, 64(%rdi)  #r15
 
   xorq %rax, %rax
@@ -25,8 +25,8 @@ set_jump2:
 
   ret 
 
-.global long_jump2    #Restaura la el estado reservado
-long_jump2:
+.global longjmp2    #Restaura el estado reservado 
+longjmp2:
 
   #restaurando registros
   
@@ -35,16 +35,16 @@ long_jump2:
   movq 16(%rdi), %rax           
   movq %rax, (%rsp)    #rsp
   
-  #Proposito general "callee safe"
-  movq   (%rdi), %rbx  #rbp
+  #Proposito general "callee saved"
+  movq   (%rdi), %rbx  #rbx   
   movq 24(%rdi), %r10  #r10
   movq 32(%rdi), %r11  #r11
   movq 40(%rdi), %r12  #r12
   movq 48(%rdi), %r13  #r13
-  movq 54(%rdi), %r14  #r14
+  movq 56(%rdi), %r14  #r14
   movq 64(%rdi), %r15  #r15
   
-  #retorno del salto (No debe de ser 0)
+  #retorno del salto (No debe ser 0)
   
   cmp $0, %rsi; 
   jnz noCero
