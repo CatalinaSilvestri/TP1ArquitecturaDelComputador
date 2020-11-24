723b9057231fff1ecdf7262f47dd76fc49217031
Joaquin Caporalini <joaquicaporalini@gmail.com>
2020-11-24 10:30:05 -0300

    Nueva y mejorada medicion del tiempo

--- plancha3/ejer7-8.c
+++ plancha3/ejer7-8.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#define BILLION 1E9
 #include <stdlib.h>
 #include <time.h>
 
@@ -25,9 +26,11 @@ void sum(float *a, float *b, int len);
 void sum_simd(float *a, float *b, int len);
 
 int main(){
-    time_t start, finish;
+    //time_t start, finish;
+    struct timespec start, finish;
     srand(time(NULL));
-    int len = 10000;
+    int len = 100000;
+    double dif;
 
     float a[len], b[len], c[len];
 
@@ -35,15 +38,21 @@ int main(){
     copy(a,c, len);
     rand_float(b, len);
 
-    start = clock();
+    //Caso unitario
+    clock_gettime (CLOCK_MONOTONIC_RAW , &start);   //Inico contados
     sum(a, b, len);
-    finish = clock();
-    printf("Tiempo sumas simples: %lf;", difftime(finish, start));
+    clock_gettime (CLOCK_MONOTONIC_RAW , &finish);  //Fin de contador
+    dif = (finish.tv_sec - start.tv_sec ) 
+      + (finish.tv_nsec - start.tv_nsec ) / BILLION;
+    printf("Tiempo sumas simples: %lf nanoseg;", dif);
 
-    start = clock();
+    //Caso paquetes
+    clock_gettime (CLOCK_MONOTONIC_RAW , &start);   //Inico contados
     sum_simd(c, b, len);
-    finish = clock();
-    printf("Tiempo sumas paquetes: %lf\n", difftime(finish, start));
+    clock_gettime (CLOCK_MONOTONIC_RAW , &finish);  //Fin de contador
+    dif = (finish.tv_sec - start.tv_sec ) 
+      + (finish.tv_nsec - start.tv_nsec ) / BILLION;
+    printf("Tiempo sumas paquetes: %lf nanoseg\n", dif);
     
     return 0;
 }
